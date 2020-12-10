96c6bc2db351d4858d0ff15b7211f9d215035389
Lucas Gomes <201810451@acad.unijuazeiro.edu.br>
2020-12-10 17:37:41 -0300

    add build project to ci workflow

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Conheça a Unijuazeiro
+          Conheça a Unijuazeirio
         </a>
       </header>
     </div>
